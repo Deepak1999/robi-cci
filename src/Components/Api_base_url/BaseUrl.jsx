6c46b8451f04e4da1bc8429cc6ddfca60b6f806f
Deepak1999 <deepakmuz151@gmail.com>
2024-09-27 12:38:35 +0530

    commit

--- src/Components/Api_base_url/BaseUrl.jsx
+++ src/Components/Api_base_url/BaseUrl.jsx
@@ -1,4 +1,4 @@
-const Api_base_url = "http://192.168.1.88:5535"
-// const Api_base_url = "http://10.101.5.61:5535"
+// const Api_base_url = "http://192.168.1.88:5535"
+const Api_base_url = "https://10.101.108.91:8443"
 
 export default Api_base_url
